07dce8f03e7710412482f6878b6d66eebf961953
Андрей Никишов <nickishov99@bk.ru>
2022-04-26 22:35:14 +0300

    Добавлена сигнатура функции Copy

--- Array_Copy/Program.cs
+++ Array_Copy/Program.cs
@@ -4,9 +4,14 @@ namespace Array_Copy
 {
     class Program
     {
+        public void Copy(Array first, Array second, int offset)
+        {
+
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int[] a = new int[5];
         }
     }
 }
