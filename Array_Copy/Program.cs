a37feb7131cd08ce7065ee64cfdbe68b60329d52
Андрей Никишов <nickishov99@bk.ru>
2022-04-26 23:39:30 +0300

    Добавлен пользовательский ввод/вывод

--- Array_Copy/Program.cs
+++ Array_Copy/Program.cs
@@ -10,8 +10,26 @@ namespace Array_Copy
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            int[] a = new int[5];
+            //Ввод пользовательских данных
+            Console.Write("Введите элементы массива, который хотите изменить, через запятую --> ");
+            string first_str = Console.ReadLine();
+            Console.Write("Введите элементы второго массива через запятую --> ");
+            string second_str = Console.ReadLine();
+            Console.Write("Введите индекс, начиная с которого необходимо заменить элементы массива --> ");
+            int index = Convert.ToInt32(Console.ReadLine());
+
+            //Получение массивов из строк
+            Array first = first_str.Split(", ");
+            Array second = second_str.Split(", ");
+
+            //Вывод полученного массива
+            Console.Write("Полученный массив --> ");
+            for (int i = 0; i < first.Length; i++)
+            {
+                Console.Write(first.GetValue(i));
+                if (i != first.Length - 1)
+                    Console.Write(", ");
+            }
         }
     }
 }
